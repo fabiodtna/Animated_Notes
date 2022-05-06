092a345a782ed125a2ef1362cdef6a73b197a551
fabiodtna <sansdtna@gmail.com>
2022-05-06 10:07:41 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useEffect } from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
+
 
 export default function App() {
+
+  const larAnimation = useRef(new Animated.Value(35)).current;
+  const altAnimation = useRef(new Animated.Value(10)).current;
+
+
+  useEffect(()=> {
+
+    // tipos de animação 
+    // Animated.parallel(animações juntas executando no mesmo tempo)
+    // Animated.sequence(animação em sequencia uma finaliza outra começa)
+    // Animated.timing(animação e o tempo que ira durar)
+
+    Animated.sequence([
+
+          Animated.timing(larAnimation,{
+            toValue:100,
+            duration:2000,
+            useNativeDriver: false
+          }),
+
+          Animated.timing(altAnimation,{
+            toValue:100,
+            duration:3000,
+            useNativeDriver:false
+          })
+
+    ]).start(() => alert("Hello There"));
+     
+
+  },[])
+
+  let porcentagemaltura = altAnimation.interpolate({
+    inputRange:[0,100],
+    outputRange:['0%', '100%']
+  })
+
+  let porcentagemlargura = larAnimation.interpolate({
+    inputRange:[0, 100],
+    outputRange: ['0%', '100%']
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Animated.View
+          style={{
+            width:porcentagemlargura,
+            height:porcentagemaltura,
+            backgroundColor:'#4169e1',
+            justifyContent:'center'      
+          }}
+        >
+            <Text style={{
+              textAlign:'center', fontSize:20, color:'#fff'
+            }}>Carregando...</Text>
+        </Animated.View>
     </View>
   );
 }
